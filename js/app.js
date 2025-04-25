932aeb2e7d201959c4db61f5038a0c652a03e1e5
zzse <125266206+yoshi182023@users.noreply.github.com>
2025-04-25 13:42:57 -0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ import { GoalView } from "./views/GoalView.js";
 import { GoalController } from "./controllers/GoalController.js";
 
 document.addEventListener("DOMContentLoaded", () => {
-  // Create instances of model, view, and controller
+ 
   const model = new GoalModel();
   const view = new GoalView();
   const controller = new GoalController(model, view);
